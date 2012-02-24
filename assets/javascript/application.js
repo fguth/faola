6e2d56f69befe5a4d6768fca605b6fcc380f747f
Matias H. Leidemer <matiasleidemer@gmail.com>
2012-02-23 23:26:40 -0200

    whos your daddy?

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -7,3 +7,42 @@ $("#borders li > a").live('click', function (event) {
 	$(this).addClass("current");
 	return false;
 });
+
+var app = {};
+
+app.color = "c64777";
+app.radius = 4;
+
+app.setColor = function(color) {
+  app.color = color;
+  app.generate();
+}
+
+app.setRadius = function(radius) {
+  app.radius = radius;
+  app.generate();
+}
+
+app.generate = function() {
+  var parser = new (less.Parser)();
+  var source =  "@color:#" + app.color + "; ";
+  source += "@BorderRadius:" + app.radius + "px; ";
+  source += "@import 'assets/faola/faola.less';"
+  
+  parser.parse(source, function(err, tree) { 
+    var css = tree.toCSS();
+    $("#faola-inline-raw").html(css);
+  });
+  
+}
+
+jQuery(function() {
+  $("#basecolor").blur(function(event) { 
+    app.setColor($(this).val()) 
+  });
+  
+  $("#borders li a").click(function(event) { 
+    app.setRadius($(this).attr("data-val")) 
+  });
+  
+})
